b99e06dbecbfb39f27cef3e6d319291e1ed2c723
Fezz <jonnicwolf@gmail.com>
2024-05-05 20:31:47 -0400

    Update App component with styled Container and remove index.css
    The changes include:
    
    * In App.jsx, the return statement has been updated to render a `Container` component instead of the previous fragment. The `Container` component

--- heart_of_the_cards/src/App.jsx
+++ heart_of_the_cards/src/App.jsx
@@ -4,10 +4,21 @@ import styled from 'styled-components';
 function App() {
 
   return (
-    <>
-      Heart of the Cards
-    </>
-  )
-}
+    <Container>
+      yoyyoyoyo
+    </Container>
+  );
+};
 
-export default App
+const Container = styled.div`
+  height: 102vh;
+  width: 102vw;
+  display: flex;
+  flex-direction: column;
+  background-color: #669897;
+  overflow: hidden;
+  padding: 15px;
+  transform: translate(-10px, -10px)
+`;
+
+export default App;
