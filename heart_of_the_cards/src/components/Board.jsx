6dec86efe60d59afd39c787319aadd55b926fa6d
Fezz <jonnicwolf@gmail.com>
2024-05-07 15:42:24 -0400

    "Add Inquery component to Board"
    This commit adds the Inquery component from './forms/Inquery' to the Board component. The Inquery component is added as a child of the Container styled component, and is positioned using flex-

--- heart_of_the_cards/src/components/Board.jsx
+++ heart_of_the_cards/src/components/Board.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 import Eye from '../components/p5/Eye';
+import Inquery from './forms/Inquery';
 
 const Board = () => {
   return (
@@ -9,12 +10,14 @@ const Board = () => {
       <EyeContainer>
         <Eye />
       </EyeContainer>
+      <Inquery />
     </Container>
   );
 };
 
 const Container = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   height: 100vw;
---
+++ heart_of_the_cards/src/components/forms/Inquery.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Inquery = () => {
+  return (
+    <Query />
+  );
+};
+
+const Query = styled.input`
+  width: 70vw;
+  
+`;
+
+export default Inquery;
