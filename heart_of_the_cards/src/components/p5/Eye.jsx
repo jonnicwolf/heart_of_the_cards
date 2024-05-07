5c243d52fbaa76d1bf7250d14605783ec06d4047
Fezz <jonnicwolf@gmail.com>
2024-05-07 06:52:08 -0400

    adds eye p5 project

---
+++ heart_of_the_cards/src/components/p5/Eye.jsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useRef, useCallback } from 'react';
+import p5 from 'p5';
+
+const Eye = () => {
+  const sketchRef = useRef();
+
+  const sketch = useCallback((p) => {
+    let angleA, angleB, angleC, reverse = 0;
+
+    p.setup = () => p.createCanvas(100,200,p.WEBGL);
+
+    p.draw = () => {
+      p.push();
+      p.fill('#f7ebed')
+      p.ellipse(0, 0, 75, 200, 4);
+
+      p.rotateZ(reverse);
+      p.fill('#b25385');
+      p.ellipse(0, 0, 65, 65, 6);
+
+      p.fill('#699897');
+      p.circle(0, 0, 50);
+      p.fill('gold')
+      p.circle(0, 0, 20);
+      p.pop();
+
+      p.line(-40, 0, -50, 0);
+      p.line(-35, -15, -50, -20);
+      p.line(-35, 15, -50, 20);
+
+      p.push();
+      p.noFill();
+      p.rotateZ(angleA);
+      p.stroke('white');
+      p.triangle(15, -10, 0, 15, -15, -10);
+      p.pop();
+
+      p.push();
+      p.noFill();
+      p.rotateZ(angleB);
+      p.rotateY(angleB);
+      p.stroke('#db8aae');
+      p.triangle(15, -10, 0, 15, -15, -10);
+      p.pop();
+
+      p.push();
+      p.noFill();
+      p.rotateZ(angleC);
+      p.rotateX(angleC);
+      p.stroke('#db8aae')
+      p.triangle(15, -10, 0, 15, -15, -10);
+      p.pop();
+
+      p.angleA += 0.1;
+      p.angleB -= 0.1;
+      p.angleC += 0.1;
+      p.reverse -= 0.01;
+
+      p.strokeWeight(2);
+    };
+  });
+
+  useEffect(() => {
+    const p5Canvas = new p5(sketch, sketch_ref.current);
+    return () => p5Canvas.remove();
+  }, []);
+
+  return <div ref={sketch_ref} />;
+};
+
+export default Eye;
