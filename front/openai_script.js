734b93bfd27217d52685140ef5878fc0fe6e872a
Fezz <jonnicwolf@gmail.com>
2024-05-24 23:17:40 -0400

    refactors openai to node from py

--- front/openai_script.js
+++ front/openai_script.js
@@ -1,4 +1,30 @@
-import { config } from "dotenv"
-config()
+import { OpenAI } from 'openai'
 
-console.log(process.env.API_URL)
+async function run() {
+  try {
+    const { config } = await import('dotenv');
+    config();
+
+    const openai = new OpenAI({
+      apiKey: process.env.OPENAI_API_KEY,
+    });
+
+    const chatCompletion = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: 'user', content: 'hello world' }],
+    });
+
+    console.log(chatCompletion.choices[0].message.content);
+  } catch (error) {
+    if (error instanceof OpenAI.APIError) {
+      console.error(error.status);
+      console.error(error.message);
+      console.error(error.code);
+      console.error(error.type);
+    } else {
+      console.log(error);
+    }
+  }
+}
+
+run();
