4c86564639d715803ae23c13ffc42da67aee8a6e
Fezz <jonnicwolf@gmail.com>
2024-05-25 21:31:31 -0400

    "Added three new functions to users.js: get_allUsers, get_userByID, and put_newUser; renamed existing function to match new naming convention."

--- back/queries/users.js
+++ back/queries/users.js
@@ -1,10 +1,33 @@
 const db = require('../db/dbConfig.js');
 
-const getAllUsers = async () => {
+const get_allUsers = async () => {
   try {
     const users = await db.any('SELECT * FROM users');
     return users }
-  catch (err) {
-    console.error(err) };
+  catch (error) {
+    console.error(error) };
 };
 
+const get_userByID = async (id) => {
+  try {
+    const user = await db.one(`SELECT * FROM users WHERE id=${id}`);
+    return user;
+  }
+  catch (error) {
+    console.error(error);
+  };
+};
+
+
+
+ const put_newUser = async (id ,newUser) => {
+  const { username, email, password_hash } = newUser;
+
+  try {
+    const user = await db.one(`UPDATE users SET username=$1, email=$2, password_hash=$3 WHERE id='${id}' RETURNING *`, [username, email, password_hash,]);
+    return user;
+  }
+  catch (error) {
+    console.error(error);
+  }
+ }
