8953c7f13d7c5937c22790ef655dd46e5d72a229
Fezz <jonnicwolf@gmail.com>
2024-05-25 23:05:20 -0400

    "Updated dependencies, added users controller, and modified routes and responses in app.js"

--- back/app.js
+++ back/app.js
@@ -1,18 +1,18 @@
 // DEPENDENCIES
-const cors = require('cors')
-const express = require('express')
+const cors = require('cors');
+const express = require('express');
+const users = require('./controllers/users_controller');
 
 // CONFIGURATION
-const app = express()
-// const = require('')
-// const = require('')
+const app = express();
 
 // MIDDLEWARE
 app.use(cors());
 app.use(express.json());
+app.use('/users', users)
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Wamnin!')
 });
 
 app.get('*', ((req, res) => {
