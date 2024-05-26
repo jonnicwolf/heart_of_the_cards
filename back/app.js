cc7de103cdd99450ea960ea6e594974982b3424f
Fezz <jonnicwolf@gmail.com>
2024-05-26 01:03:29 -0400

    housekeeping

--- back/app.js
+++ back/app.js
@@ -16,7 +16,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('*', ((req, res) => {
-  res.status(404).send("<h1>404 Page Not Found<h1/>")
+  res.status(404).send("404 Page Not Found")
 }));
 
 // EXPORT
