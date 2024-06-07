8cfdff42c40cc8a2d55e23bc859ff7edc7cdea4f
Fezz <jonnicwolf@gmail.com>
2024-06-07 11:12:58 -0400

    "Added Firebase initialization code with environment variables"

---
+++ front/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+const app = firebase.initializeApp({
+  apiKey:process.env.VITE_FIREBASE_API_KEY,
+  authDomain:process.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectID:process.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket:process.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId:process.env.VITE_FIREBASE_APP_ID,
+  measurementId:process.env.VITE_FIREBASE_MEASUREMENT_ID,
+})
