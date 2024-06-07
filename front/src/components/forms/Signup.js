fecfcf2519c1c23f7f58f796072cc7ce78e813bf
Fezz <jonnicwolf@gmail.com>
2024-06-07 11:23:35 -0400

    "Updated firebase.js and package-lock.json files, added bootstrap and react-bootstrap dependencies to package.json"

---
+++ front/src/components/forms/Signup.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Form, Card, Button } from 'react-bootstrap';
+
+const Signup = () => {
+  return (
+    <>
+      <Card>
+        <h2>Sign Up</h2>
+        <Form>
+
+        </Form>
+      </Card>
+      <div>Already have an account? Log In Link</div>
+    </>
+  )
+}
+
+const Container = styled.div``;
+
+
+export default Signup;
