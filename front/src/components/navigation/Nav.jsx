379af2ec15de78f1d30d0d78ae154031b777e164
Fezz <jonnicwolf@gmail.com>
2024-06-12 05:31:40 -0400

    "Added media queries for responsive styling to Nav component"

--- front/src/components/navigation/Nav.jsx
+++ front/src/components/navigation/Nav.jsx
@@ -49,12 +49,14 @@ const Container = styled.footer`
   z-index: 2;
 `;
 const MenuSwitch = styled.button`
-  width: 10%;
   height: 100%;
   color: #e1c4ca;
   background: none;
   border: none;
   justify-self: flex-start;
+  @media only screen and (max-width: 500px) {
+    font-size: 10px;
+  }
 `;
 const LogoContainer = styled.div`
   display: flex;
@@ -63,6 +65,11 @@ const LogoContainer = styled.div`
   font-family: Bagnard;
   line-height: 20px;
   transform: translate(5vw,-10px);
+  @media only screen and (max-width: 500px) {
+    font-size: 1rem;
+    line-height: 15px;
+    transform: translate(30px,-5px);
+  }
 `;
 const Secrets = styled.div`
   display: flex;
@@ -76,6 +83,11 @@ const OfThe_Vertical = styled.span`
   text-align: center;
   line-height: 15px;
   transform: translateY(-5px);
+  @media only screen and (max-width: 500px) {
+    font-size: 0.5rem;
+    line-height: 5px;
+    transform: none;
+  }
 `;
 
 export default Nav;
