af3a372d36e23a7d829648b2650a9640a41d4d2e
Fezz <jonnicwolf@gmail.com>
2024-06-21 01:53:55 -0400

    "Updated Firebase config and added analytics logging to Board component"

--- client/firebase.js
+++ client/firebase.js
@@ -15,7 +15,7 @@ import {
 const app = initializeApp({
   apiKey:            import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain:        import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectID:         import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  projectId:         import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket:     import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId:             import.meta.env.VITE_FIREBASE_APP_ID,
@@ -23,7 +23,7 @@ const app = initializeApp({
 });
 
 const auth = getAuth(app);
-const analytics = getAnalytics(app)
+const analytics = getAnalytics(app);
 
 export {
   auth,
