97426e670975f2e7711b33689397f026d3510dd4
Fezz <jonnicwolf@gmail.com>
2024-07-03 01:27:41 -0400

    housekeeping

---
+++ server/db/seed.sql
@@ -0,0 +1,7 @@
+INSERT INTO users (username, email, public_key, private_key)
+VALUES
+  ('alice', 'alice@example.com', 'PUBLIC_KEY_ALICE', 'PRIVATE_KEY_ALICE'),
+  ('bob', 'bob@example.com', 'PUBLIC_KEY_BOB', 'PRIVATE_KEY_BOB'),
+  ('charlie', 'charlie@example.com', 'PUBLIC_KEY_CHARLIE', 'PRIVATE_KEY_CHARLIE'),
+  ('david', 'david@example.com', 'PUBLIC_KEY_DAVID', 'PRIVATE_KEY_DAVID'),
+  ('eve', 'eve@example.com', 'PUBLIC_KEY_EVE', 'PRIVATE_KEY_EVE');
