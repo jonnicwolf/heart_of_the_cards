47b15297bfa4f49326cac01fbb8cf2ddfad9b434
Fezz <jonnicwolf@gmail.com>
2024-07-05 07:27:19 -0400

    "Updated Nav component: added showMenu prop to LogoContainer, moved Logout button inside SlideOutMenu, and added text elements for Home, History, and Logout buttons."

--- client/src/components/navigation/Nav.jsx
+++ client/src/components/navigation/Nav.jsx
@@ -33,7 +33,7 @@ const Nav = () => {
   return (
     <Container>
       <MenuLogoWarp>
-        <LogoContainer onClick={handleClick}>
+        <LogoContainer onClick={handleClick} showMenu={showMenu}>
           <span>ESP</span>
           <span>ERI</span>
         </LogoContainer>
@@ -41,16 +41,19 @@ const Nav = () => {
         <SlideOutMenu showMenu={showMenu}>
           <HomeButton>
             <Home src='https://img.icons8.com/?size=100&id=xZbsecl9NwAy&format=png&color=FFFFFF' alt=''/>
+            <HomeText>Home</HomeText>
           </HomeButton>
           <HistoryButton>
             <History src="https://img.icons8.com/?size=100&id=hZ5zdXjC6tJ3&format=png&color=FFFFFF" alt="" />
+            <HistoryText>History</HistoryText>
           </HistoryButton>
+          <MenuSwitch onClick={handleLogout}>
+            <Logout_Btn />
+            <LogoutText>Logout</LogoutText>
+          </MenuSwitch>
         </SlideOutMenu>
       </MenuLogoWarp>
 
-      <MenuSwitch onClick={handleLogout}>
-        <Logout_Btn />
-      </MenuSwitch>
     </Container>
   );
 };
@@ -70,7 +73,8 @@ const MenuLogoWarp = styled.div`
   display: flex;
   gap: 2vw;
   align-items: center;
-  width: 23%;
+  // justify-content: space-between;
+  width: 30%;
 `
 const MenuSwitch = styled.button`
   height: 1G00%;
@@ -87,13 +91,12 @@ const LogoContainer = styled.div`
   flex-direction: column;
   font-size: 2rem;
   font-family: Bagnard;
-  line-height: 25px;
+  line-height: ${props => props.showMenu ? '35px' : '25px'};
+  transition: line-height 0.3s ease-in-out;
+  cursor: pointer;
   @media only screen and (max-width: 500px) {
     font-size: 1rem;
     line-height: 15px;
-  }
-  @media only screen and (min-width: 1000px) {
-    font-size: 2.1rem;
   }
 `;
 const SlideOutMenu = styled.div`
@@ -107,18 +110,37 @@ const SlideOutMenu = styled.div`
   transition: all 0.5s ease-in-out;
 `;
 const Home = styled.img`
-  height: 2.8rem;
+  height: 2rem;
 `;
 const HomeButton = styled.button`
   background: none;
   border: none;
 `;
+const HomeText = styled.span`
+  color: white;
+  font-size: 0.7rem;
+  font-family: Bagnard;
+`;
 const History = styled.img`
-  height: 2.5rem;
+  height: 2rem;
 `;
 const HistoryButton = styled.button`
   background: none;
   border: none;
 `;
+const HistoryText = styled.span`
+  color: white;
+  font-size: 0.7rem;
+  font-family: Bagnard;
+  padding: 0;
+  margin: 0;
+`;
+const LogoutText = styled.span`
+  color: white;
+  font-size: 0.7rem;
+  font-family: Bagnard;
+  padding: 0;
+  margin: 0;
+`
 
 export default Nav;
