f829c5c18d3afba727e7d9c2369a5ceb0b55c5bc
Fezz <jonnicwolf@gmail.com>
2024-07-19 09:17:33 -0400

    "Added imports, modified JSX structure, and added styled components for HistoryPage"

--- client/src/pages/HistoryPage.jsx
+++ client/src/pages/HistoryPage.jsx
@@ -1,10 +1,16 @@
+import { Page } from 'openai/pagination.mjs';
 import React from 'react';
 import styled from 'styled-components';
 
 export default function HistoryPage() {
   return (
     <Container>
-      HistoryPage
+      <ContentWrap>
+        <PageTitle>Past Readings</PageTitle>
+        <div>
+          HistoryPage
+        </div>
+      </ContentWrap>
     </Container>
   );
 };
@@ -12,5 +18,21 @@ export default function HistoryPage() {
 const Container = styled.div`
   height: 100vw;
   width: 100vw;
-  max-width: 1500px;
+  display: flex;
+  justify-content: center;
+  background-color: #fce6c5;
 `;
+const ContentWrap = styled.div`
+  width: 90%;
+  gap: 2vw;
+  max-width: 1500px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 5vh;
+`;
+const PageTitle = styled.h1`
+  font-size: 2rem;
+  font-family: Bagnard;
+
+`
