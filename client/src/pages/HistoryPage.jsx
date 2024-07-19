25ad4f27d88c979f8a374f23ee8ed0e429c670f7
Fezz <jonnicwolf@gmail.com>
2024-07-19 09:28:38 -0400

    "Updated styles for ContentWrap and PageTitle components in HistoryPage"

--- client/src/pages/HistoryPage.jsx
+++ client/src/pages/HistoryPage.jsx
@@ -29,10 +29,15 @@ const ContentWrap = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding-top: 5vh;
+  padding-top: 1vh;
 `;
 const PageTitle = styled.h1`
   font-size: 2rem;
   font-family: Bagnard;
-
-`
+  --s: 5px;
+  padding: var(--s);
+  border: calc(5*var(--s)) solid #0000 ;
+  outline: 1px solid #000;
+  outline-offset: calc(-3*var(--s));
+  background: conic-gradient(from 90deg at 1px 1px,#0000 25%,#000 0);
+`;
