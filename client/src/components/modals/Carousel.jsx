250520a9ec508f5b1eb2031a88f37e3a1dc52b34
Fezz <jonnicwolf@gmail.com>
2024-07-23 12:57:32 -0400

    "Added state and functionality to Carousel component to handle card navigation"

--- client/src/components/modals/Carousel.jsx
+++ client/src/components/modals/Carousel.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
-export default function Carousel() {
+export default function Carousel({ cards }) {
+  const [currentCard, setCurrentCard] = useState(0);
+
+  function handleNext () {
+    currentCard >= cards.length-1 ? setCurrentCard(0) : setCurrentCard(currentCard + 1);
+  };
+  function handlePrev () {
+    currentCard <= 0 ? setCurrentCard(cards.length-1) : setCurrentCard(currentCard - 1);
+  };
+  function goTo (cardNumber) {
+    setCurrentCard(cardNumber);
+  }
+
   return (
-    <Container>Carousel</Container>
+    <Container>
+      {cards[currentCard]}
+      <Pagination>
+        {cards.map(_,i => <PageCircleButton onClick={() => goTo(i)}/> )}
+      </Pagination>
+    </Container>
   );
 };
 
@@ -12,4 +29,14 @@ const Container = styled.div`
   height: 50%;
   border: 1px solid red;
   background-color: #fce6c5;
-`
+`;
+const Pagination = styled.div`
+  display: flex;
+  justify-content: space-between;
+  gap: 2vw;
+`;
+const PageCircleButton = styled.button`
+  height: 1vw;
+  width: 1vw;
+  border-radius: 50%;
+`;
