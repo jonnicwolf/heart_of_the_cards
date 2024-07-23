1eaec34ac3f7c3a3af119011ea2897da0a97b182
Fezz <jonnicwolf@gmail.com>
2024-07-23 18:26:26 -0400

    "Added 'tracksMouse' prop to Eye component and updated irisX and irisY calculations to use it"

--- client/src/components/p5/Eye.jsx
+++ client/src/components/p5/Eye.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useCallback } from 'react';
 import p5 from 'p5';
 
-const Eye = ({ width, height }) => {
+const Eye = ({ width, height, tracksMouse }) => {
   const sketch_ref = useRef();
 
   const sketch = useCallback((p) => {
@@ -16,8 +16,8 @@ const Eye = ({ width, height }) => {
     };
 
     p.draw = () => {
-      const irisX = p.map(p.mouseX, 0, width, (width / 60)*-1, p.width / 60);
-      const irisY = p.map(p.mouseY, 0, height, (width / 24)*-1, p.height / 36);
+      const irisX = p.map(tracksMouse ? p.mouseX : 50, 0, width, (width / 60)*-1, p.width / 60);
+      const irisY = p.map(tracksMouse ? p.mouseY : 60, 0, height, (width / 24)*-1, p.height / 36);
       p.background(0, 0.9);
 
       p.push();
