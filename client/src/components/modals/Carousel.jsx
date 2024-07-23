19e28a570906fb4df86f779eb27a960833c5c44c
Fezz <jonnicwolf@gmail.com>
2024-07-23 10:20:07 -0400

    "Added Carousel component with basic React and styled-components implementation"

---
+++ client/src/components/modals/Carousel.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function Carousel() {
+  return (
+    <div>Carousel</div>
+  );
+};
+
+const Container = styled.div`
+  width: 50%;
+  height: 50%;
+  background-color: #fce6c5;
+`
