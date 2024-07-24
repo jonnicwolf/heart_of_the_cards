946dfd51723901197ed92d66181339c66adfa8c8
Fezz <jonnicwolf@gmail.com>
2024-07-23 21:44:04 -0400

    "Update Modal background color opacity from 0.5 to 0.2"

--- client/src/components/modals/Modal.jsx
+++ client/src/components/modals/Modal.jsx
@@ -22,7 +22,7 @@ export default function Modal() {
 const Container = styled.div`
   width: 100vw;
   height: 100vh;
-  background-color: rgba(0,0,0,0.5);
+  background-color: rgba(0,0,0,0.2);
   display: flex;
   justify-content: center;
   padding-top: 10vh;
