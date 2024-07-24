8cce794bb6924c479b869b2740319a9838326aec
Fezz <jonnicwolf@gmail.com>
2024-07-23 20:28:20 -0400

    "Updated HIWCard component with new content and styles"

--- client/src/components/modals/HIWCard.jsx
+++ client/src/components/modals/HIWCard.jsx
@@ -3,10 +3,37 @@ import styled from 'styled-components';
 
 export default function HIWCard() {
   return (
-    <Container>HIWCard</Container>
+    <Container>
+      <Title>How it works</Title>
+      <Example></Example>
+      <Message>
+        
+Enter your question into the provided box, and you'll receive a meticulously detailed tarot reading. This reading will include a thorough breakdown of each card drawn, along with in-depth interpretations to help you gain a deeper understanding of the insights and guidance they offer.
+      </Message>
+    </Container>
   );
 };
 
 const Container = styled.div`
-
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  padding-top: 30px;
+  gap: 40px;
+  font-family: Bagnard;
+`;
+const Title = styled.h1`
+  font-family: Bagnard;
+  font-size: 3rem;
+  text-align: center;
+`;
+const Example = styled.video`
+  width: 80%;
+  border: 1px solid red;
 `
+const Message = styled.p`
+  font-family: Bagnard;
+  text-align: center;
+  padding: 30px;
+`;
