cb61c40e3491390237d2e84dee34b46a6a8e5eb2
Fezz <jonnicwolf@gmail.com>
2024-08-28 14:20:23 -0400

    Converts forgot password page

--- client/src/pages/ForgotPasswordPage.tsx
+++ client/src/pages/ForgotPasswordPage.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { FC } from 'react';
 import styled from 'styled-components';
 import ForgotPassword from '../components/forms/ForgotPassword';
 import GraphicCard from '../components/GraphicCard';
 
-export default function ForgotPasswordPage() {
-  const windowWidth = window.innerWidth < 850;
+interface Props {
+  windowWidth: boolean
+};
+
+const ForgotPasswordPage: FC = () => {
+  const windowWidth: boolean = window.innerWidth < 850;
 
   return (
     <Container>
@@ -19,7 +23,9 @@ export default function ForgotPasswordPage() {
 const Container = styled.div`
   display: flex;
 `;
-const SignupCard = styled.div`
+const SignupCard = styled.div<Props>`
   width: ${props => props.windowWidth ? '100vw' : '50vw'};
   height: 100vh;
 `;
+
+export default ForgotPasswordPage;
